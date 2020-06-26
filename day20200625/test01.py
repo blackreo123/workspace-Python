19abe66cd8200f9c72e615674f805d88152da3bf
JIHA <blackreo123@naver.com>
2020-06-26 09:11:25 +0900

    변수와 자료형

---
+++ day20200625/test01.py
@@ -0,0 +1,20 @@
+str1 = "hello,"
+str2 = "world!"
+
+str3 = "SCIT"
+str4 = "master"
+print(str1, end=" ")
+print(str2)
+#print(str3 + 38 + str4)
+print(str3, 38, str4, sep="*")
+
+
+ipt = input("입력하세요: ")
+print(ipt)
+
+a = 10 
+
+tryNum = 0 # camel 표기법
+try_num = 0 # snake 표기법
+
+
---
+++ day20200625/test02.py
@@ -0,0 +1,25 @@
+a = 1010
+b = 0b1010
+c = 0o1010
+d = 0x1010
+
+print(a,b,c,d,sep=",")
+print("type(a): ", type(a))
+print("type(b): ", type(b))
+print("type(c): ", type(c))
+print("type(d): ", type(d))
+
+print("bin(a): ", bin(a))
+print("oct(b): ", oct(b))
+print("hex(c): ", hex(c))
+print("bin(d): ", bin(d))
+
+e = 3.14159265
+f= 3.0e8
+
+print("e:", e)
+print("f:", f)
+print("type(f): ", type(f))
+
+directory = r"C:\test"
+print(directory)
