a9396b0fbd369cc448f91fdd20244c237db311da
JIHA <blackreo123@naver.com>
2020-06-29 14:46:50 +0900

    20200629

---
+++ day20200629/test01.py
@@ -0,0 +1,28 @@
+print(ord("a"))
+print(chr(65))
+
+t= True
+f= False
+
+if 1:
+    print("참입니다!")
+else:
+    print("거짓입니다!")
+
+if 0:
+    print("참입니다!")
+else:
+    print("거짓입니다!")
+
+if "hello":
+    print("참입니다!")
+else:
+    print("거짓입니다!")
+
+print(type(int("3")))
+print(type(float("3.14")))
+print(type(str(3)))
+
+print(10/3)
+print(10//3) #몫 구하기 연산자
+print(10%3)
---
+++ day20200629/test02.py
@@ -0,0 +1,3 @@
+str= "Life is too short, You need Python"
+print(str[0] + str[1]+str[2]+str[3])
+print(str[-1] + str[-2]+str[-3]+str[-4])
